3d4d9d370442e5742c33ceeed806e0ae9e076838
josdem <joseluis.delacruz@gmail.com>
2023-11-15 07:44:28 -0500

    #5 Adding recipe fragment bindings

--- app/src/main/java/com/josdem/fruitypedia/RecipeFragment.kt
+++ app/src/main/java/com/josdem/fruitypedia/RecipeFragment.kt
@@ -5,19 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.josdem.fruitypedia.databinding.FragmentRecipeBinding
 
-import com.josdem.fruitypedia.databinding.FragmentRecipeBuilding
 
 class RecipeFragment : Fragment() {
 
-    private var _binding: FragmentRecipeBuilding? = null
+    private var _binding: FragmentRecipeBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentRecipeBuilding.inflate(inflater, container, false)
+        _binding = FragmentRecipeBinding.inflate(inflater, container, false)
         return binding.root
     }
 
