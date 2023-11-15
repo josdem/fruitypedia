abf40b66302a6403ceb030abcfd75b60de4b3ba0
josdem <joseluis.delacruz@gmail.com>
2023-11-15 11:07:09 -0500

    #5 Adding category getter and setter

--- app/src/main/java/com/josdem/fruitypedia/model/Category.kt
+++ app/src/main/java/com/josdem/fruitypedia/model/Category.kt
@@ -1,4 +1,22 @@
 package com.josdem.fruitypedia.model
 
 class Category {
+    private var id: Int = 0
+    private var name: String = ""
+
+    fun getId(): Int {
+        return id
+    }
+
+    fun getName(): String {
+        return name
+    }
+
+    fun setId(id: Int) {
+        this.id = id
+    }
+
+    fun setName(name: String) {
+        this.name = name
+    }
 }
