d60e630ad672c845f728741ac14679fcc871a1d3
josdem <joseluis.delacruz@gmail.com>
2023-11-15 13:56:59 -0500

    #6 Simplyfing category data class

--- app/src/main/java/com/josdem/fruitypedia/model/Category.kt
+++ app/src/main/java/com/josdem/fruitypedia/model/Category.kt
@@ -1,22 +1,6 @@
 package com.josdem.fruitypedia.model
 
-class Category {
-    private var id: Int = 0
-    private var name: String = ""
-
-    fun getId(): Int {
-        return id
-    }
-
-    fun getName(): String {
-        return name
-    }
-
-    fun setId(id: Int) {
-        this.id = id
-    }
-
-    fun setName(name: String) {
-        this.name = name
-    }
-}
+data class Category(
+    val id: Int,
+    val name: String
+)
