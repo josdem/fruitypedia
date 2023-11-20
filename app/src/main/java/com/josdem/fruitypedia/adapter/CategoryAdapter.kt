cb9a0c5d046adc0d5c00c9f4a747ce391f44a9a7
josdem <joseluis.delacruz@gmail.com>
2023-11-20 08:54:12 -0500

    #9 Adding adapter

---
+++ app/src/main/java/com/josdem/fruitypedia/adapter/CategoryAdapter.kt
@@ -0,0 +1,10 @@
+package com.josdem.fruitypedia.adapter
+
+import android.content.Context
+import android.widget.ArrayAdapter
+import com.josdem.fruitypedia.model.Category
+
+
+class CategoryAdapter(context: Context, resource: Int) : ArrayAdapter<Category>(context, resource) {
+
+}
