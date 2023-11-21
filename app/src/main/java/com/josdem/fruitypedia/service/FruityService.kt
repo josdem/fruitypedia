4413656bea4c65e9eb471a8ed9c8946db1e2f7d3
josdem <joseluis.delacruz@gmail.com>
2023-11-20 19:12:30 -0500

    #9 Adding beverage endpoint

--- app/src/main/java/com/josdem/fruitypedia/service/FruityService.kt
+++ app/src/main/java/com/josdem/fruitypedia/service/FruityService.kt
@@ -1,7 +1,7 @@
 package com.josdem.fruitypedia.service
 
+import com.josdem.fruitypedia.model.Beverage
 import com.josdem.fruitypedia.model.Category
-import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
 
@@ -10,4 +10,7 @@ interface FruityService {
 
     @GET("/categories/en")
     suspend fun getCategories(): Response<List<Category>>
+
+    @GET("/categories/{id}/beverages")
+    suspend fun getBeverages(id: Int): Response<List<Beverage>>
 }
