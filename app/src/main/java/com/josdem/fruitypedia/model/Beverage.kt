c0056f5bc0d81aa1bc3c78b85890b86e99c4d936
josdem <joseluis.delacruz@gmail.com>
2023-11-20 19:10:44 -0500

    #9 Adding beverage DTO

---
+++ app/src/main/java/com/josdem/fruitypedia/model/Beverage.kt
@@ -0,0 +1,9 @@
+package com.josdem.fruitypedia.model
+
+data class Beverage(
+    val id: Int,
+    val name: String,
+    val ingredients: String,
+    val recipe: String,
+    val image: String
+)
