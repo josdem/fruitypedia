b20f10cc50ae7ac5a656ada1fd011d5b661d4333
josdem <joseluis.delacruz@gmail.com>
2023-11-21 06:02:23 -0500

    #9 Adding beverage layout

--- app/src/main/java/com/josdem/fruitypedia/RecipeFragment.kt
+++ app/src/main/java/com/josdem/fruitypedia/RecipeFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.josdem.fruitypedia.databinding.FragmentRecipeBinding
+import com.josdem.fruitypedia.model.Beverage
 import com.josdem.fruitypedia.service.FruityService
 import com.josdem.fruitypedia.service.RetrofitHelper
 import com.josdem.fruitypedia.state.ApplicationState
@@ -33,9 +34,16 @@ class RecipeFragment : Fragment() {
         val fruityService = RetrofitHelper.getInstance().create(FruityService::class.java)
 
         MainScope().launch {
-            val result = fruityService.getBeverage(101)
+            val result =
+                fruityService.getBeverage(ApplicationState.getValue("currentBeverage") as Int)
             Log.d("beverage: ", result.body().toString())
+            displayResults(result.body())
         }
+    }
+
+    private fun displayResults(beverage: Beverage?) {
+
+
     }
 
     override fun onDestroyView() {
