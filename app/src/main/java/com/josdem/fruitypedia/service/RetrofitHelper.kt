9aa85a3cd90eebbd9e14d41edaf27abc299c3530
josdem <joseluis.delacruz@gmail.com>
2023-11-23 07:18:10 -0500

    #25 Changing base url to private access modifier

--- app/src/main/java/com/josdem/fruitypedia/service/RetrofitHelper.kt
+++ app/src/main/java/com/josdem/fruitypedia/service/RetrofitHelper.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitHelper {
 
-    const val BASE_URL = "https://webflux.josdem.io/"
+    private const val BASE_URL = "https://webflux.josdem.io/"
 
     fun getInstance(): Retrofit {
         return Retrofit.Builder().baseUrl(BASE_URL)
