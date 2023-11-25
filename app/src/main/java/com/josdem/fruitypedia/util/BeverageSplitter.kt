7d69e10600fc518ec127b0ef0fe476776d20821f
josdem <joseluis.delacruz@gmail.com>
2023-11-25 10:42:02 -0500

    #19 Formatting code

--- app/src/main/java/com/josdem/fruitypedia/util/BeverageSplitter.kt
+++ app/src/main/java/com/josdem/fruitypedia/util/BeverageSplitter.kt
@@ -1,9 +1,7 @@
 package com.josdem.fruitypedia.util
 
 class BeverageSplitter {
-
     companion object {
-
         fun split(ingredients: String): String {
             val stringBuilder = StringBuilder()
             val token = ingredients.split(",")
--- app/src/test/java/com/josdem/fruitypedia/BeverageSplitterTest.kt
+++ app/src/test/java/com/josdem/fruitypedia/BeverageSplitterTest.kt
@@ -5,11 +5,10 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class BeverageSplitterTest {
-
     @Test
     fun shouldSplitIngredients() {
         val ingredients = "5 Blueberries,1 Apple,2 Oranges,Honey to taste"
         val result = BeverageSplitter.split(ingredients)
-        assertEquals("5 Blueberries" + "\n" + "1 Apple" + "\n" +"2 Oranges" + "\n" + "Honey to taste" + "\n", result)
+        assertEquals("5 Blueberries" + "\n" + "1 Apple" + "\n" + "2 Oranges" + "\n" + "Honey to taste" + "\n", result)
     }
 }
