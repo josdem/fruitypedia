7aa5549b78d874f29d70ed635c34e1db0cda3f3d
josdem <joseluis.delacruz@gmail.com>
2023-12-09 08:49:57 -0500

    #262 Getting first test passing

--- app/src/test/java/com/josdem/fruitypedia/CategoryFragmentTest.kt
+++ app/src/test/java/com/josdem/fruitypedia/CategoryFragmentTest.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import com.josdem.fruitypedia.state.ApplicationState
+import io.mockk.MockKAnnotations
 import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.mockkStatic
 import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
 
 class CategoryFragmentTest {
@@ -18,11 +21,16 @@ class CategoryFragmentTest {
     @MockK
     private lateinit var bundle: Bundle
 
+    @InjectMockKs
+    private var categoryFragment = CategoryFragment()
+
+    @Before
+    fun setUp() = MockKAnnotations.init(this)
+
     @Test
     fun shouldValidateCategoryFragment() {
         mockkStatic(Log::class)
         every { Log.d(any(), any()) } returns 0
-        val categoryFragment = CategoryFragment()
         categoryFragment.onViewCreated(view, bundle)
         assertEquals(categoryFragment, ApplicationState.getValue("categoryFragment"))
     }
