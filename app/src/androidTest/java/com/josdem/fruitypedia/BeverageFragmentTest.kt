dd0a246c819d3da00f91d6f7c97afeb74446f110
josdem <joseluis.delacruz@gmail.com>
2024-07-16 13:59:40 -0400

    #70 Adding view validation

--- app/src/androidTest/java/com/josdem/fruitypedia/BeverageFragmentTest.kt
+++ app/src/androidTest/java/com/josdem/fruitypedia/BeverageFragmentTest.kt
@@ -2,6 +2,9 @@ package com.josdem.fruitypedia
 
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.lifecycle.Lifecycle
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.LayoutAssertions.noEllipsizedText
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.josdem.fruitypedia.state.ApplicationState
 import org.junit.Before
@@ -22,5 +25,6 @@ class BeverageFragmentTest {
             initialState = Lifecycle.State.INITIALIZED
         )
         scenario.moveToState(Lifecycle.State.RESUMED)
+        onView(withId(R.id.listViewBeverages)).check(noEllipsizedText())
     }
 }
