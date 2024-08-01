80b8334e542a5cbe77dee62f20cdcda80c6887f0
josdem <joseluis.delacruz@gmail.com>
2024-08-01 12:39:50 -0400

    #82 Validating expected beverages

--- app/src/test/java/com/josdem/fruitypedia/FruityServiceTest.kt
+++ app/src/test/java/com/josdem/fruitypedia/FruityServiceTest.kt
@@ -1,5 +1,6 @@
 package com.josdem.fruitypedia
 
+import com.josdem.fruitypedia.model.Beverage
 import com.josdem.fruitypedia.model.Category
 import com.josdem.fruitypedia.service.FruityService
 import com.josdem.fruitypedia.service.RetrofitHelper
@@ -10,6 +11,7 @@ import org.junit.Test
 import retrofit2.Response
 
 const val LANGUAGE = "en"
+const val CATEGORY = 5
 
 class FruityServiceTest {
     private val fruityService: FruityService =
@@ -19,6 +21,10 @@ class FruityServiceTest {
         return fruityService.getCategories(LANGUAGE)
     }
 
+    private suspend fun getBeverages(): Response<List<Beverage>> {
+        return fruityService.getBeverages(CATEGORY)
+    }
+
     @Test
     fun shouldGetCategories() =
         runTest {
@@ -26,5 +32,14 @@ class FruityServiceTest {
             val categories: List<Category>? = response.body()
             assertTrue(response.isSuccessful)
             assertEquals(4, categories?.size)
+        }
+
+    @Test
+    fun shouldGetBeverages() =
+        runTest {
+            val response = getBeverages()
+            val beverages: List<Beverage>? = response.body()
+            assertTrue(response.isSuccessful)
+            assertEquals(14, beverages?.size)
         }
 }
