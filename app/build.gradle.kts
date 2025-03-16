5bc67b3d39e44660a5988b25954cc8074eb9784d
K M Merajul Arefin <merajularefin@gmail.com>
2025-03-16 17:52:40 +0600

    Upgrade gradle plugin and other libraries to the latest versions
    Issue: #90, #91
    Problem: Gradle plugin and other libraries are not in their latest
    versions
    Solution: Upgrade gradle plugins and other libraries to the latest
    versions
    
    - upgrade gradle plugin to the latest version
    - upgrade other libraries to their latest versions
    - upgrade targetSdk and compileSdk to 35
    - upgrade gradle to 8.11.1
    - remove android.defaults.buildfeatures.buildconfig as it is deprecated
    - add support for edge-to-edge because of SDK 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,17 +6,16 @@ plugins {
     id("com.diffplug.spotless") version "6.22.0"
 }
 
-val testIntegrationVersion = "1.6.1"
-val fragmentVersion = "1.8.1"
+val fragmentVersion = "1.8.6"
 
 android {
     namespace = "com.josdem.fruitypedia"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.josdem.fruitypedia"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 8
         versionName = "1.2.0"
 
@@ -41,6 +40,7 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        buildConfig = true
     }
 }
 
@@ -61,13 +61,13 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.navigation:navigation-fragment-ktx:2.8.9")
     implementation("androidx.navigation:navigation-ui-ktx:2.8.9")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation(platform("com.google.firebase:firebase-bom:33.10.0"))
     implementation("com.google.firebase:firebase-analytics")
 
     testImplementation("junit:junit:4.13.2")
-    testImplementation("io.mockk:mockk:1.13.8")
+    testImplementation("io.mockk:mockk:1.13.17")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
 
@@ -80,6 +80,6 @@ dependencies {
     debugImplementation("androidx.tracing:tracing:1.2.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     //noinspection GradleDependency
-    androidTestImplementation("androidx.test:runner:$testIntegrationVersion")
-    androidTestImplementation("androidx.test:rules:$testIntegrationVersion")
+    androidTestImplementation("androidx.test:runner:1.6.2")
+    androidTestImplementation("androidx.test:rules:1.6.1")
 }
